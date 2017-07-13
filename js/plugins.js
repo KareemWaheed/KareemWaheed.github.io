c7c7a7be3453557e5560830df214465f6f04d73a
Kareem Waheed <kareemwaheed94@gmail.com>
2017-07-13 22:10:10 +0200

    First Commit

---
+++ js/plugins.js
@@ -0,0 +1,13 @@
+/*global $,alert,console */
+$(function () {
+    'use strict';
+    $("html").niceScroll({
+        cursorcolor: '#03285b',
+        cursorwidth: '10px',
+        scrollspeed: '60',
+        cursorborderradius: '0px',
+        cursorborder: '0px',
+        cursoropacitymin: '.4'
+
+    });
+});
